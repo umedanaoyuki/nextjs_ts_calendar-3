6937c5c9927eb109c1d6fc7b279b662154f6be63
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-06 01:52:57 +0900

    Userスキーマの作成

---
+++ app/utils/schemaModels.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = mongoose.models.User || mongoose.model("User", UserSchema);
