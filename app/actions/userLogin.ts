b00890a54b2b03612eded14daa9688838ea03c34
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-06 19:34:36 +0900

    ログイン時のエラーメッセージを表示する

--- app/actions/userLogin.ts
+++ app/actions/userLogin.ts
@@ -3,7 +3,10 @@
 import connectDB from "../utils/database";
 import { UserModel } from "../utils/schemaModels";
 
-export const userLogin = async (formData) => {
+export const userLogin = async (
+  prevState: { message: string },
+  formData: FormData
+) => {
   const userData = {
     email: formData.get("email"),
     password: formData.get("password"),
@@ -11,9 +14,18 @@ export const userLogin = async (formData) => {
 
   try {
     await connectDB();
-    const savedData = await UserModel.findOne({ email: userData.email });
-    console.log({ savedData });
-    return { message: "ログイン成功" };
+    const savedUserData = await UserModel.findOne({ email: userData.email });
+    console.log({ savedUserData });
+
+    if (savedUserData) {
+      if (userData.password === savedUserData.password) {
+        return { message: "ログイン成功" };
+      } else {
+        return { message: "パスワードが間違っています" };
+      }
+    } else {
+      return { message: "エラー：ユーザー登録をしてください" };
+    }
   } catch {
     return { message: "ログインに失敗しました" };
   }
--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { userLogin } from "@/app/actions/userLogin";
+import { useActionState } from "react";
 
 const initialState = {
   message: "",
 };
 
 const Login = () => {
+  const [state, formAction] = useActionState(userLogin, initialState);
   return (
     <div>
-      <form action={userLogin}>
+      <form action={formAction}>
         <input type="text" name="email" placeholder="メールアドレス" required />
         <input type="text" name="password" placeholder="パスワード" required />
+        {state && <h3>{state.message}</h3>}
         <button>ログイン</button>
       </form>
     </div>
