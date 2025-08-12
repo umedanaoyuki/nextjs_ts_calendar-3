dbae9da96f4bfb1438308619091a152ef12347d3
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-12 19:43:50 +0900

    refactor: ログインが確立されている場合は、処理をスキップする

--- app/utils/database.ts
+++ app/utils/database.ts
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGODB_URI!);
+    isConnected = true;
     console.log("MongoDB接続成功");
-  } catch {
+  } catch (err) {
     console.log("MongoDB接続失敗");
-    throw new Error();
+    throw err;
   }
 };
 
