a5565aafc85f1a79432bb0bb42dffd41a5406896
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-13 21:17:35 +0900

    feat: ナビゲーション箇所の月の変更機能

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -43,6 +43,7 @@ const CalendarPage = () => {
         <CalendarNavigation
           setCurrentDate={setCurrentDate}
           addSchedule={addSchedule}
+          currentDate={currentDate}
         />
         <div className="mx-0.5 my-2 grid flex-1 grid-cols-7 overflow-hidden rounded-2xl bg-white shadow-xl sm:m-4">
           {weekDays.map((day, i) => {
--- app/components/organisms/CalendarNavigation.tsx
+++ app/components/organisms/CalendarNavigation.tsx
@@ -1,15 +1,17 @@
 import { Schedule } from "@/app/types/calendar";
-import { addMonths } from "date-fns";
+import { addMonths, getMonth, getYear } from "date-fns";
 import { Dispatch, SetStateAction, useState } from "react";
 
 type PropsType = {
   setCurrentDate: Dispatch<SetStateAction<Date>>;
   addSchedule: (schedule: Schedule) => void;
+  currentDate: Date;
 };
 
 export const CalendarNavigation = ({
   setCurrentDate,
   addSchedule,
+  currentDate,
 }: PropsType) => {
   const [isOpen, setIsOpen] = useState(false);
   const closeModal = () => setIsOpen(false);
@@ -34,17 +36,25 @@ export const CalendarNavigation = ({
           今日
         </button>
         <div className="flex items-center rounded-lg px-4 py-2 shadow-sm">
-          <button className="flex h-5 w-5 items-center justify-center rounded-full bg-white transition-colors duration-500 hover:bg-purple-100">
+          <button
+            className="flex h-5 w-5 items-center justify-center rounded-full bg-white transition-colors duration-500 hover:bg-purple-100"
+            onClick={changePrevMonth}
+          >
             ←
           </button>
-          <button className="flex h-5 w-5 items-center justify-center rounded-full bg-white transition-colors duration-500 hover:bg-purple-100">
+          <button
+            className="flex h-5 w-5 items-center justify-center rounded-full bg-white transition-colors duration-500 hover:bg-purple-100"
+            onClick={changeNextMonth}
+          >
             →
           </button>
-          <span>2024年3月</span>
+          <span>{`${getYear(currentDate)}年${
+            getMonth(currentDate) + 1
+          }月`}</span>
         </div>
       </div>
       <div className="flex items-center justify-center gap-4">
-        <p>予定作成</p>
+        <button onClick={() => setIsOpen(true)}>予定作成</button>
         <p>テストユーザー</p>
         <p>ログアウト</p>
       </div>
