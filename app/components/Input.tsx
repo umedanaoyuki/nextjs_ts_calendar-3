13a02ad436bc55e52c243ca00c2494bbe0d7978c
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-13 19:16:11 +0900

    refactor: componentPropsの使用

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,25 +1,9 @@
-type InputProps = {
-  id: string;
-  name: string;
-  type: string;
-  required: boolean;
-  placeholder: string;
-};
+import { ComponentProps } from "react";
 
-export const Input = ({
-  id,
-  name,
-  type,
-  required,
-  placeholder,
-}: InputProps) => {
+export const Input = (props: ComponentProps<"input">) => {
   return (
     <input
-      id={id}
-      name={name}
-      type={type}
-      required={required}
-      placeholder={placeholder}
+      {...props}
       className="appearance-none rounded-[5px] relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
     />
   );
