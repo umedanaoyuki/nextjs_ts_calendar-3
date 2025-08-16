e213d60796a935eaad953183cea1c97bc9ba85e8
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-16 14:50:51 +0900

    feat: tokenの有用性を調べる処理を追加

--- middleware.js
+++ middleware.js
@@ -1,11 +1,19 @@
 import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
 export async function middleware(request) {
   const token = request.cookies.get("token")?.value;
 
   if (!token) return NextResponse.redirect(new URL("/user/login", request.url));
 
-  return NextResponse.next();
+  try {
+    const secretKey = new TextEncoder().encode(process.env.SECRET_KEY);
+    await jwtVerify(token, secretKey);
+    return NextResponse.next();
+  } catch {
+    console.log("jwtVerify エラー");
+    return NextResponse.redirect(new URL("/user/login", request.url));
+  }
 }
 
 export const config = {
