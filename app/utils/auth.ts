8501b7b9ee455149bc0493b2dcb391398bc4ee90
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-17 19:44:48 +0900

    refactor: updateTokenを２つの関数に分ける

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -35,22 +35,55 @@ export const setTokenCookie = async ({
   cookie.set("token", token, config);
 };
 
-export const updateToken = async (token: string) => {
+// export const updateToken = async (token: string) => {
+//   const secretKey = new TextEncoder().encode(process.env.SECRET_KEY);
+//   const { payload } = await jwtVerify(token, secretKey);
+
+//   // payloadの型を適切に変換
+//   const tokenPayload = {
+//     email: payload.email as string,
+//   };
+
+//   const newToken = await generateToken({
+//     payload: tokenPayload,
+//     secretKey,
+//   });
+
+//   const response = NextResponse.next();
+
+//   response.cookies.set("token", newToken, {
+//     name: "token",
+//     value: newToken,
+//     maxAge: 60 * 60 * 2,
+//   });
+
+//   return response;
+// };
+
+export const refreshToken = async (token: string): Promise<string> => {
   const secretKey = new TextEncoder().encode(process.env.SECRET_KEY);
   const { payload } = await jwtVerify(token, secretKey);
 
-  const newToken = await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setExpirationTime("2h")
-    .sign(secretKey);
+  // payloadの型を適切に変換
+  const tokenPayload = {
+    email: payload.email as string,
+  };
 
-  const response = NextResponse.next();
-
-  response.cookies.set("token", newToken, {
-    name: "token",
-    value: newToken,
-    maxAge: 60 * 60 * 2,
+  const newToken = await generateToken({
+    payload: tokenPayload,
+    secretKey,
   });
 
+  return newToken;
+};
+
+export const withTokenCookie = (token: string) => {
+  const response = NextResponse.next();
+  response.cookies.set("token", token, {
+    maxAge: 60 * 60 * 2,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
   return response;
 };
--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { updateToken } from "./app/utils/auth";
+import { refreshToken, withTokenCookie } from "./app/utils/auth";
 
 export async function middleware(request) {
   const token = request.cookies.get("token")?.value;
@@ -7,7 +7,8 @@ export async function middleware(request) {
   if (!token) return NextResponse.redirect(new URL("/user/login", request.url));
 
   try {
-    return updateToken(token);
+    const newToken = await refreshToken(token);
+    return withTokenCookie(newToken);
   } catch {
     console.log("jwtVerify エラー");
     return NextResponse.redirect(new URL("/user/login", request.url));
