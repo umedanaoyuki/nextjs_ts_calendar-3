71226d1b2117b5891081088567b53d04f7d8f72f
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-25 04:51:08 +0900

    refactor: 不要なファイルの削除

--- app/page.tsx
+++ app/page.tsx
@@ -63,52 +63,6 @@ const CalendarPage = () => {
           deleteSchedule={deleteSchedule}
           changeSchedule={changeSchedule}
         />
-        {/* {days.map((day) => {
-          return (
-            <div
-              key={day}
-              className={cn(
-                "flex flex-col items-center border-r border-b border-solid border-gray-100 p-0 pt-1 sm:items-start sm:p-2 sm:pt-2",
-                day > 31 ? "bg-gray-50/50" : "bg-white",
-                todayDate === day && "ring-2 ring-purple-400 ring-inset"
-              )}
-            >
-              <span
-                className={cn(
-                  "flex h-6 w-6 items-center justify-center rounded-full text-sm sm:h-8 sm:w-8 sm:text-base",
-                  todayDate === day && "bg-purple-100 font-bold text-purple-600"
-                )}
-              >
-                {day <= 31 ? day : day - 31}
-              </span>
-              <div className="flex w-full flex-col gap-1">
-                {events[day as keyof typeof events]?.map((event, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className={cn("flex gap-1 rounded-md p-1 text-xs", {
-                        "bg-blue-100 text-blue-700": event.color === "blue",
-                        "bg-green-100 text-green-700": event.color === "green",
-                        "bg-purple-100 text-purple-700":
-                          event.color === "purple",
-                        "bg-red-100 text-red-700": event.color === "red",
-                        "bg-orange-100 text-orange-700":
-                          event.color === "orange",
-                        "bg-indigo-100 text-indigo-700":
-                          event.color === "indigo",
-                      })}
-                    >
-                      <span className="hidden sm:inline">{event.time}</span>
-                      <span className="hidden truncate sm:inline">
-                        {event.title}
-                      </span>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })} */}
       </div>
     </div>
   );
