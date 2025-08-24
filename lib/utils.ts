c0a3ad547e934997d2f25f4f9aced5c31dd7c0e9
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-25 04:53:51 +0900

    refactor: mainと記述内容を合わせる

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,6 @@ import clsx from "clsx";
 import { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export const cn = (...inputs: ClassValue[]) => {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-};
+}
