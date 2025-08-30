fbb04013a4c17efc2190fddd7d5112f6673230a2
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-08-31 03:03:38 +0900

    feat: パスワードをvisibleにする機能の追加

--- app/components/molecules/SignupForm.tsx
+++ app/components/molecules/SignupForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useHookFormAction } from "@next-safe-action/adapter-react-hook-form/hooks";
 import { sigup } from "@/app/actions/signup";
@@ -17,9 +17,12 @@ import {
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import Link from "next/link";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const SignupForm = () => {
   const router = useRouter();
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
   const { form, action, handleSubmitWithAction } = useHookFormAction(
     sigup,
@@ -94,7 +97,7 @@ const SignupForm = () => {
                 <FormLabel>パスワード</FormLabel>
                 <FormControl>
                   <Input
-                    type="password"
+                    type={passwordVisible ? "text" : "password"}
                     placeholder="8文字以上30文字以内で入力してください"
                     {...field}
                   />
@@ -113,7 +116,7 @@ const SignupForm = () => {
                 <FormLabel>パスワード（確認）</FormLabel>
                 <FormControl>
                   <Input
-                    type="password"
+                    type={passwordVisible ? "text" : "password"}
                     placeholder="8文字以上30文字以内で入力してください"
                     {...field}
                   />
@@ -123,6 +126,19 @@ const SignupForm = () => {
             </>
           )}
         />
+        <div className="flex items-center gap-2">
+          <Checkbox
+            id="passwordVisible"
+            name="passwordVisible"
+            checked={passwordVisible}
+            onCheckedChange={(checked: boolean) => {
+              return setPasswordVisible(checked);
+            }}
+          />
+          <label htmlFor="passwordVisible" className="text-sm">
+            パスワードを表示
+          </label>
+        </div>
         <Button type="submit" disabled={action.isPending} className="">
           {action.isPending ? "作成中..." : "アカウントを作成"}
         </Button>
